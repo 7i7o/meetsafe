fb19b9b4a276c0ebeaef176a6700bc9255d31301
7i7o <mparij@gmail.com>
2023-08-16 19:40:17 -0300

    update landing ui + login link

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -12,17 +12,6 @@ const MainPage: React.FC = () => {
     <div
       className={`flex h-screen flex-col items-center justify-center text-gray-200 ${varela.className}`}
     >
-      <div className="absolute left-0 top-0 ml-8 mt-8 flex space-x-8 text-lg">
-        <a href="#" className="hover:underline">
-          Solutions
-        </a>
-        <a href="#" className="hover:underline">
-          Why MeetSafe
-        </a>
-        <Link href="/login">
-          Login
-        </Link>
-      </div>
       <div
         className="absolute inset-0"
         style={{
@@ -32,21 +21,38 @@ const MainPage: React.FC = () => {
           zIndex: "-1",
         }}
       />
-      <div className="flex flex-col items-center space-y-4">
+      <div className="absolute left-0 top-0 ml-8 mt-8 flex space-x-8 text-lg">
+        <a href="#" className="hover:underline">
+          Solutions
+        </a>
+        <a href="#" className="hover:underline">
+          Why MeetSafe
+        </a>
+      </div>
+      <div className="flex flex-col items-center space-y-8">
         <Image src={logo} alt="Logo" width={logoSize} height={logoSize} />
-        <h1 className={`text-6xl font-extralight`}>MeetSafe</h1>
+        <h1 className={`text-5xl font-extralight`}>MeetSafe</h1>
         <p className="text-2xl">
           Connecting Needs, Ensure Safety, Choose your Privacy
         </p>
-        <p className="w-[580px] text-center text-xl">
+        <p className="w-[500px] text-center text-xl">
           A decentralized platform where you can satisfy your needs and
           professionals can provide their services safely and 100% anunymously,
           if you want.
         </p>
-        <button className="shadow- rounded-full bg-gradient-to-tr from-[#b578b0] to-[#c18c92] px-24 py-3 text-2xl font-bold text-white hover:bg-blue-600">
+        {/* <button */}
+        <Link
+          href="login"
+          className="shadow- rounded-full bg-gradient-to-tr from-[#b578b0] to-[#c18c92] px-24 py-3 text-2xl font-bold text-white hover:bg-blue-600"
+          style={{ boxShadow: "0 8px #3a3a39" }}
+        >
           Join MeetSafe
-        </button>
-        <button className="rounded-full bg-gray-300 px-4 py-2 font-bold text-gray-700 hover:bg-gray-400">
+        </Link>
+        {/* </button> */}
+        <button
+          className="rounded-full bg-[#fce93a] px-10 py-3 text-2xl font-bold text-[#2d2d2c] hover:bg-gray-400"
+          style={{ boxShadow: "0 8px #3a3a39" }}
+        >
           enter as spectator
         </button>
       </div>
