7c59f2c95242828d47fdc07726db5185f35a9015
NicolasBiondini <nicolasbiondini29@gmail.com>
2023-08-17 15:52:40 -0300

    profile9

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/ban-types */
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "~/components/NavBar";
 import banner from "~/assets/banner.svg";
 import profileimage from "~/assets/profile.svg";
@@ -11,8 +11,8 @@ import placeholderimage from "~/assets/placeholder.svg";
 import star from "~/assets/star.svg";
 
 function Profile() {
-  //const initial = 0;
-  // const [selected, setSelected] = useState(initial);
+  const initial = 0;
+  const [selected, setSelected] = useState(initial);
 
   return (
     <section className="flex h-full w-full flex-col gap-2 bg-gradient-to-r from-black to-[#545452] text-gray-200">
@@ -84,127 +84,157 @@ function Profile() {
               centuries, but also
             </p>
           </div>
-          {
-            // user profile
-          }
-          <div className=""></div>
-          <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
-            <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
-              <h5 className="text-3xl font-semibold">Languages</h5>
-              <div className="flex flex-col gap-2">
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-              </div>
+          <div className="ml-20 flex w-[85%] flex-row items-center justify-center gap-3 border-b-2 border-solid border-black text-2xl font-semibold text-[#545452]">
+            <div
+              onClick={() => setSelected(0)}
+              className={`flex h-12 w-1/3 items-center justify-center rounded-tl-xl rounded-tr-xl border-2 border-b-0 border-solid border-[#545452] ${
+                selected == 0 && "bg-[#545452] text-gray-200"
+              } cursor-pointer hover:bg-[#545452] hover:text-gray-200`}
+            >
+              Profile
+            </div>
+            <div
+              onClick={() => setSelected(1)}
+              className={`flex h-12 w-1/3 items-center justify-center rounded-tl-xl rounded-tr-xl border-2 border-b-0 border-solid border-[#545452] ${
+                selected == 1 && "bg-[#545452] text-gray-200"
+              } cursor-pointer hover:bg-[#545452] hover:text-gray-200`}
+            >
+              Channels
+            </div>
+            <div
+              onClick={() => setSelected(2)}
+              className={`flex h-12 w-1/3 items-center justify-center rounded-tl-xl rounded-tr-xl border-2 border-b-0 border-solid border-[#545452] ${
+                selected == 2 && "bg-[#545452] text-gray-200"
+              } cursor-pointer hover:bg-[#545452] hover:text-gray-200`}
+            >
+              Plans
             </div>
           </div>
-          <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
-            <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
-              <h5 className="text-3xl font-semibold">
-                Certification and education
-              </h5>
-              <div className="flex flex-col gap-2">
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-              </div>
-            </div>
-          </div>
-          <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
-            <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
-              <h5 className="text-3xl font-semibold">Experience</h5>
-              <div className="flex flex-col gap-2">
-                <p>Lorem Ipsum</p>
-                <p>Lorem Ipsum</p>
-              </div>
-            </div>
-          </div>
-          <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
-            <div className="flex h-full w-full flex-row justify-start gap-20 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
-              <div className=" w-2/3 rounded-2xl bg-white px-6 py-5">
-                <Image
-                  src={placeholderimage as string}
-                  alt="placeholder"
-                  height={250}
-                  width={250}
-                />
-              </div>
-              <div className="flex flex-col gap-7 pt-5">
-                <h5 className="text-3xl font-semibold">Portfolio</h5>
-                <div className="flex flex-col gap-2">
-                  <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industrys
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries,
-                    but also
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
-            <div className="flex h-full w-full flex-col justify-start gap-5 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
-              <h5 className="text-3xl font-semibold">Raitings</h5>
-              <div className="flex flex-row items-center">
-                <Image
-                  alt="star"
-                  src={star as string}
-                  width={100}
-                  height={100}
-                />
-                <p className="flex flex-row items-center gap-2 text-4xl font-bold">
-                  4.0<span className="text-2xl font-thin">{"(3)"}</span>
-                </p>
-              </div>
-              <div className="flex flex-col gap-3">
-                <div className="flex w-full flex-row items-center justify-center gap-2">
-                  <span className="text-lg font-semibold">5</span>
-                  <div className="h-2.5 w-full rounded-full bg-white">
-                    <div
-                      className="h-2.5 rounded-full bg-yellow-400"
-                      style={{ width: "90%" }}
-                    ></div>
-                  </div>
-                </div>
-                <div className="flex w-full flex-row items-center justify-center gap-2">
-                  <span className="text-lg font-semibold">4</span>
-                  <div className="h-2.5 w-full rounded-full bg-white">
-                    <div
-                      className="h-2.5 rounded-full bg-yellow-400"
-                      style={{ width: "50%" }}
-                    ></div>
-                  </div>
-                </div>
-                <div className="flex w-full flex-row items-center justify-center gap-2">
-                  <span className="text-lg font-semibold">3</span>
-                  <div className="h-2.5 w-full rounded-full bg-white">
-                    <div
-                      className="h-2.5 rounded-full bg-yellow-400"
-                      style={{ width: "0%" }}
-                    ></div>
-                  </div>
-                </div>
-                <div className="flex w-full flex-row items-center justify-center gap-2">
-                  <span className="text-lg font-semibold">2</span>
-                  <div className="h-2.5 w-full rounded-full bg-white">
-                    <div
-                      className="h-2.5 rounded-full bg-yellow-400"
-                      style={{ width: "0%" }}
-                    ></div>
-                  </div>
-                </div>
-                <div className="flex w-full flex-row items-center justify-center gap-2">
-                  <span className="text-lg font-semibold">1</span>
-                  <div className="h-2.5 w-full rounded-full bg-white">
-                    <div
-                      className="h-2.5 rounded-full bg-yellow-400"
-                      style={{ width: "30%" }}
-                    ></div>
+          {selected == 0 ? (
+            <div className="flex flex-col gap-9">
+              <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
+                <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
+                  <h5 className="text-3xl font-semibold">Languages</h5>
+                  <div className="flex flex-col gap-2">
+                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
                   </div>
                 </div>
               </div>
+              <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
+                <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
+                  <h5 className="text-3xl font-semibold">
+                    Certification and education
+                  </h5>
+                  <div className="flex flex-col gap-2">
+                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
+                  </div>
+                </div>
+              </div>
+              <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
+                <div className="flex h-full w-full flex-col gap-7 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
+                  <h5 className="text-3xl font-semibold">Experience</h5>
+                  <div className="flex flex-col gap-2">
+                    <p>Lorem Ipsum</p>
+                    <p>Lorem Ipsum</p>
+                  </div>
+                </div>
+              </div>
+              <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
+                <div className="flex h-full w-full flex-row justify-start gap-20 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
+                  <div className=" w-2/3 rounded-2xl bg-white px-6 py-5">
+                    <Image
+                      src={placeholderimage as string}
+                      alt="placeholder"
+                      height={250}
+                      width={250}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-7 pt-5">
+                    <h5 className="text-3xl font-semibold">Portfolio</h5>
+                    <div className="flex flex-col gap-2">
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the industrys
+                        standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it
+                        to make a type specimen book. It has survived not only
+                        five centuries, but also
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="ml-20 flex w-[85%] flex-col items-center justify-center">
+                <div className="flex h-full w-full flex-col justify-start gap-5 rounded-3xl bg-gray-200 px-10 py-8 text-[#545452]">
+                  <h5 className="text-3xl font-semibold">Raitings</h5>
+                  <div className="flex flex-row items-center">
+                    <Image
+                      alt="star"
+                      src={star as string}
+                      width={100}
+                      height={100}
+                    />
+                    <p className="flex flex-row items-center gap-2 text-4xl font-bold">
+                      4.0<span className="text-2xl font-thin">{"(3)"}</span>
+                    </p>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <div className="flex w-full flex-row items-center justify-center gap-2">
+                      <span className="text-lg font-semibold">5</span>
+                      <div className="h-2.5 w-full rounded-full bg-white">
+                        <div
+                          className="h-2.5 rounded-full bg-yellow-400"
+                          style={{ width: "90%" }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-center gap-2">
+                      <span className="text-lg font-semibold">4</span>
+                      <div className="h-2.5 w-full rounded-full bg-white">
+                        <div
+                          className="h-2.5 rounded-full bg-yellow-400"
+                          style={{ width: "50%" }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-center gap-2">
+                      <span className="text-lg font-semibold">3</span>
+                      <div className="h-2.5 w-full rounded-full bg-white">
+                        <div
+                          className="h-2.5 rounded-full bg-yellow-400"
+                          style={{ width: "0%" }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-center gap-2">
+                      <span className="text-lg font-semibold">2</span>
+                      <div className="h-2.5 w-full rounded-full bg-white">
+                        <div
+                          className="h-2.5 rounded-full bg-yellow-400"
+                          style={{ width: "0%" }}
+                        ></div>
+                      </div>
+                    </div>
+                    <div className="flex w-full flex-row items-center justify-center gap-2">
+                      <span className="text-lg font-semibold">1</span>
+                      <div className="h-2.5 w-full rounded-full bg-white">
+                        <div
+                          className="h-2.5 rounded-full bg-yellow-400"
+                          style={{ width: "30%" }}
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          ) : selected == 1 ? (
+            <div></div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </section>
